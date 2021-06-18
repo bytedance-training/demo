38fde7eb7d123c5187b3a62ba80d2b86cc9710a5
lijun.110 <lijun.110@bytedance.com>
2021-06-18 16:47:03 +0800

    feat: with mock

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  React.useEffect(() => {
+    window.fetch('/mock/70395/goods/list').then(res => {
+      return res.json()
+    })
+    .then(res => {
+      console.log(res);
+      debugger;
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
